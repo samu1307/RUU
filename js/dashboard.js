12e9c73fb801b78ca2cb1279d3d8323aee71b0bb
Samuel Sarmiento <samusamisam@gmail.com>
2023-04-08 23:47:08 -0500

    Cambios en index y dashboard

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,2 +1,156 @@
 const d = document;
 
+//Query bodys
+const bodyDbUsers = d.querySelector('.body-db-users')
+const bodyDbCourses = d.querySelector('.body-db-courses')
+const bodyDbSnacks = d.querySelector('.body-db-snacks')
+const bodyDbProfile = d.querySelector('.body-db-profile')
+const bodyDbReport = d.querySelector('.body-db-report')
+
+//Query headers
+const headers = d.querySelectorAll('.header-db')
+const hUsers = d.querySelector('.header-db-users')
+const hCourses = d.querySelector('.header-db-courses')
+const hSnacks = d.querySelector('.header-db-snacks')
+const hProfile = d.querySelector('.header-db-profile')
+const hReport = d.querySelector('.header-db-report')
+
+//Query btnMenu
+const btnMenuUsers = d.querySelector('#menu-icon-users')
+const btnMenuCourses = d.querySelector('#menu-icon-courses')
+
+//Query NAV
+const nav = d.querySelector('.nav')
+const jc = d.querySelectorAll('.jc-s')
+const iconImg = d.querySelectorAll('.icon-img')
+const svgIcon = d.querySelectorAll('.svg-icon')
+
+//Query HTML
+const html = d.querySelector('html')
+
+
+
+
+//heightHover
+const heightHover = ()=>{
+    iconImg.forEach(e=>{
+        svgIcon.forEach(i=>{
+            let heightI = i.clientHeight
+            e.style.height = `${heightI}px`
+        })
+    })
+}
+
+//Menu users
+btnMenuUsers.addEventListener('click', ()=>{
+    if(bodyDbUsers.classList.length == 4 ){
+
+        headers.forEach(h=>{
+            h.style.position = 'absolute'
+        })
+
+        bodyDbUsers.classList.add('body-view-menu-users')
+        bodyDbCourses.classList.add('body-view-menu-courses')
+        bodyDbSnacks.classList.add('body-view-menu-snacks')
+        bodyDbProfile.classList.add('body-view-menu-profile')
+        bodyDbReport.classList.add('body-view-menu-report')
+
+        html.style.overflow = 'hidden'
+    }else{
+        bodyDbUsers.classList.remove('body-view-menu-users')
+        bodyDbCourses.classList.remove('body-view-menu-courses')
+        bodyDbSnacks.classList.remove('body-view-menu-snacks')
+        bodyDbProfile.classList.remove('body-view-menu-profile')
+        bodyDbReport.classList.remove('body-view-menu-report')
+
+        headers.forEach(h=>{
+            setTimeout(()=>{
+                h.style.position = 'fixed'
+            }, 500)
+        })
+
+        html.style.overflow = 'auto'
+    }
+})
+
+//Menu Courses
+btnMenuCourses.addEventListener('click', ()=>{
+
+    let ifCourses = bodyDbCourses.classList.value 
+    let classCourses = 'body-db-courses body-db df cxy'
+    
+    if(ifCourses == classCourses){
+
+        bodyDbCourses.classList.remove('load-profile')
+        
+        headers.forEach(h=>{
+            h.style.position = 'absolute'
+        })
+        
+        bodyDbCourses.classList.add('body-view-menu-courses1')
+        bodyDbUsers.classList.add('body-view-menu-users1')
+        bodyDbSnacks.classList.add('body-view-menu-snacks1')
+        bodyDbProfile.classList.add('body-view-menu-profile1')
+        bodyDbReport.classList.add('body-view-menu-report1')
+        
+        html.style.overflow = 'hidden'
+
+    }else{
+
+        // bodyDbCourses.classList.add('load-profile')
+
+        bodyDbUsers.classList.remove('body-view-menu-users1')
+        bodyDbCourses.classList.remove('body-view-menu-courses1')
+        bodyDbSnacks.classList.remove('body-view-menu-snacks1')
+        bodyDbProfile.classList.remove('body-view-menu-profile1')
+        bodyDbReport.classList.remove('body-view-menu-report1')
+
+        headers.forEach(h=>{
+            setTimeout(()=>{
+                h.style.position = 'fixed'
+            }, 500)
+        })
+
+        html.style.overflow = 'auto'
+    }
+})
+
+jc.forEach(e=>{
+    e.addEventListener('click', ()=>{
+
+        e.classList.add('click-sections')
+        html.style.overflow = 'auto'
+        
+        if(e.id == 'profile'){
+            bodyDbProfile.classList.add('load-profile')
+            setTimeout(()=>{
+                bodyDbProfile.style.zIndex = '70'
+            }, 2000)
+        }else if(e.id == 'course'){
+            
+        }else if(e.id == 'user'){
+
+        }else if(e.id == 'snack'){
+
+        }else if(e.id == 'report'){
+
+        }
+
+        
+        setTimeout(()=>{
+            bodyDbUsers.classList.remove('body-view-menu-users')
+            bodyDbCourses.classList.remove('body-view-menu-courses')
+            bodyDbSnacks.classList.remove('body-view-menu-snacks')
+            bodyDbProfile.classList.remove('body-view-menu-profile')
+            bodyDbReport.classList.remove('body-view-menu-report')
+            bodyDbCourses.classList.remove('load-profile')
+        }, 2000)
+    })
+})
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    heightHover();
+})
+window.addEventListener('resize', ()=>{
+    heightHover();
+})
--- js/index.js
+++ js/index.js
@@ -1,11 +1,17 @@
 const d = document;
+const head = d.querySelector('.main-head');
+const main = d.querySelector('main')
+const foot = d.querySelector('footer')
+const map = d.querySelector('.map')
+const lazyLoad = d.querySelectorAll('.lazy-load')
 const btnMenu = d.querySelector('.btn-menu');
-const menuHeader = d.querySelector('#menu-slider-header');
+const menuHeader = d.querySelector('.menu-slider-header');
 const navIndex = d.querySelector('#nav-index');
 const navNavIndex = d.querySelector('.nav-nav-index');
 const btnLogin = d.querySelector('.btn-login')
 const aSections = d.querySelectorAll('.a-sections')
 const mSlider = d.querySelector('.main-slider')
+const MainISlider = d.querySelector('.main-img-slider') 
 const imgSlider = d.querySelectorAll('img[data-spy="scroll"]')
 const btnL = d.querySelector('#row-btn-l');
 const btnR = d.querySelector('#row-btn-r');
@@ -13,14 +19,55 @@ const divSlider = d.querySelector('.img-slider');
 const videoI = d.querySelector('.videoIframe')
 const mapI = d.querySelector('.mapIframe')
 
+lazyLoad.forEach(e=>{
+    console.log(e.loading)
+})
+
+//Funcion para relacion cuadrada en elemento y en Div
+const cuadrado = ()=>{
+    aSections.forEach(e=>{
+        if(menuHeader.classList.length == 2){
+            menuHeader.style.height = `${menuHeader.clientWidth}px`
+        }else{
+            menuHeader.style.height = '0'
+        }
+        e.style.height = `${e.clientWidth}px`
+    })
+};
+
+//Designar alto de a-sections al redimencionar
+window.addEventListener('resize', cuadrado)
+
 const menuIndex = ()=>{
     btnMenu.addEventListener('click', ()=>{
-        menuHeader.classList.toggle('menu-header-close')
+        menuHeader.classList.toggle('menu-header-open')
+        head.classList.toggle('opacity-off')
+        main.classList.toggle('opacity-off')
+        foot.classList.toggle('opacity-off')
+        cuadrado();
     });
     
-    aSections.forEach(e=>{
-        e.addEventListener('click', ()=>{
-            menuHeader.classList.add('menu-header-close')
+    aSections.forEach(i=>{
+        i.addEventListener('click', ()=>{
+            head.classList.remove('opacity-off')
+            main.classList.remove('opacity-off')
+            foot.classList.remove('opacity-off')
+            setTimeout(()=>{
+                aSections.forEach(e=>{
+                    if(e.id != i.id){
+                        e.style.opacity = '0.5'
+                        e.style.scale = '1'
+                    } 
+                    else{
+                        i.style.opacity = '1'
+                        i.style.scale = '1.1'
+                    } 
+                })
+            },600)
+            setTimeout(()=>{
+                menuHeader.style.height = '0'
+                menuHeader.classList.remove('menu-header-open')
+            }, 1000)
         })
     });
 }
@@ -28,11 +75,11 @@ const menuIndex = ()=>{
 const navAnimated = ()=>{
     document.addEventListener('scroll', ()=>{
         if(scrollY < 20){
-            navIndex.classList.remove('nav-fixed')
+            navNavIndex.style.boxShadow = ''
             navNavIndex.style.padding = '5% 5% 2.5%'
             btnLogin.style.padding = '3vw 4vw'
         }else{
-            navIndex.classList.add('nav-fixed')
+            navNavIndex.style.boxShadow = '0 2px 15px rgba(0, 0, 0, .3)'
             navNavIndex.style.padding = '2.5% 5%'
             btnLogin.style.padding = '2.5vw 3.5vw'
         }
@@ -89,10 +136,31 @@ const scrollSpy = ()=>{
     imgSlider.forEach(e => observer.observe(e))
 }
 
+const lazyLoading = ()=>{
+
+    const cb = (entrys)=>{
+        entrys.forEach(i=>{
+            if(i.isIntersecting){
+                console.log(i)
+            }
+        })
+    }
+
+    const observe = new IntersectionObserver(cb, {
+        threshold: 0.1,
+        root: d
+    })
+
+    lazyLoad.forEach(e=> observe.observe(e))
+
+}
+
 scrollSpy();
+lazyLoading();
 navAnimated();
 menuIndex();
 sliderL();
 sliderR();
 
-
+//Designar alto de a-sections al cargar
+d.addEventListener('DOMContentLoaded', cuadrado)
