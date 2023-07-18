74920c02478673231f5a1da6391f8372f7a91c35
samu1307 <samusamisam@gmail.com>
2023-07-17 22:47:05 -0500

    Update-Login

--- js/login.js
+++ js/login.js
@@ -1,23 +1,57 @@
 const d = document;
 const bodySlider = d.querySelector('.left__body-slider');
+const bodyStyle = bodySlider.style;
 
-(function (){
-    let marginLeft = bodySlider.style.marginLeft;
-    console.log(bodySlider.style.marginLeft)
-    setInterval(()=>{
-        switch (marginLeft) {
-            case '':
-                console.log()   
-                marginLeft = '-100%'    
-                break;
-            case '-100%':
-                console.log(marginLeft)
-                marginLeft = '-200%'    
-                break;        
-            case '-200%':
-                console.log(marginLeft)
-                marginLeft = '0px'    
-                break;
-        }
-    }, 3000)
+/* Secciones */
+  //Card
+  const secCard = d.querySelectorAll('.left__body-slider-sec');
+  //Btn
+  const secBtn = d.querySelectorAll('.login-sec-btn');
+
+
+//Slider Animation
+setInterval(()=>{
+    switch (bodyStyle.marginLeft) {
+        case '0px':
+            setTimeout(()=>{
+                bodyStyle.marginLeft = '-100%'
+            }, 1000)
+        break;
+        case '-100%':
+            setTimeout(()=>{
+                bodyStyle.marginLeft = '-200%'
+            }, 1000)
+        break;
+        case '-200%':
+            setTimeout(()=>{
+                bodyStyle.marginLeft = '0px'
+            }, 1000)
+        break;
+    }
+}, 10000);
+
+//Scroll Spy for Btn
+(()=>{
+
+    const cb = (entries)=>{
+
+        entries.forEach(i => {
+            let di = i.target.dataset.slider;
+            if(i.isIntersecting){
+                d.querySelector(`button[data-slider="${di}"]`)
+                .classList.add('login-btn-active');
+                i.target.classList.add('login-sec-active');
+            }else{
+                d.querySelector(`button[data-slider="${di}"]`)
+                .classList.remove('login-btn-active');
+                i.target.classList.remove('login-sec-active');
+            }
+        })
+    }
+
+    const observer = new IntersectionObserver(cb, {
+        threshold: 0.8
+    })
+
+    secCard.forEach(e => observer.observe(e))
 })()
